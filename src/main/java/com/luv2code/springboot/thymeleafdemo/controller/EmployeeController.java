470a0f746b0c620f37e94c60ff159daecc9241c6
Rohit <hellorohit2014@gmail.com>
2023-06-29 10:23:26 +0530

    project intial 0.1

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,83 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController  {
+
+	private  EmployeeService employeeService;
+
+    public  EmployeeController(EmployeeService theemployeeService)
+	{
+      employeeService=theemployeeService;
+	}
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+
+		List<Employee> theEmployees = employeeService.findAll();
+
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+
+		return "employees/list-employees";
+	}
+	@GetMapping("/showFormForAdd")
+	public  String showFormForAdd(Model theModel)
+	{
+		//create model attribute to bind from data
+		Employee  theEmployee = new Employee();
+
+		theModel.addAttribute("employee" ,theEmployee);
+
+		return  "employees/employee-form";
+	}
+	@PostMapping("/save")
+	public  String saveEmployee(@ModelAttribute("employee") Employee theEmployee)
+	{
+		//save the employee
+        employeeService.save(theEmployee);
+
+		//use a redirect to prevent duplicate submission
+	   return  "redirect:/employees/list";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public  String showFormForUpdate(@RequestParam("employeeId") int theId ,Model themodel)
+	{
+// get the employee from the service
+
+		Employee theEmployee = employeeService.findById(theId);
+		//set the employee in the model to prepopulate the form
+      themodel.addAttribute("employee" ,theEmployee);
+		//send over to our form
+ return   "employees/employee-form";
+	}
+
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId)
+	{
+		//delete the employee
+         employeeService.deleteById(theId);
+		//redirect  to the  employee form
+		return  "redirect:/employees/list";
+	}
+}
+
+
+
+
+
+
+
+
+
